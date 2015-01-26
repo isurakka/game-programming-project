3ce1e114662b248d3cff83ab608f0c549e814c1d
Iiro Surakka <iiro.surakka@gmail.com>
2015-01-26 17:43:39 +0200

    Remove all code from project

--- Source/TopdownGame/Private/TopdownGame.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
-#include "TopdownGame.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopdownGame, "TopdownGame" );
-
-DEFINE_LOG_CATEGORY(LogTopdownGame)
- 
